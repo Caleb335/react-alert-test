7c4fce1e85d9fafa3d1851346758f3800b634e09
EspiraMarvin <espiramarvin@gmail.com>
2021-08-20 17:53:44 +0300

    project boilerplate

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,24 @@
 import '../styles/globals.css'
+import React from 'react'
+import Head from "next/head";
+import Message from "../components/Message";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+  <React.Fragment>
+    <Head>
+      <link rel="icon" href="#" />
+      <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+      />
+      <title>Alert</title>
+    </Head>
+    <Message>
+      <Component {...pageProps} />
+    </Message>
+  </React.Fragment>
+  )
 }
 
 export default MyApp
